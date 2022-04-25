72679fbd6fb0755fc11d6f068f29b0cee7c39a19
Manish Kumar <56536688+manishkumar102@users.noreply.github.com>
2022-04-25 14:15:12 +0530

    Add files via upload

---
+++ python code.py
@@ -0,0 +1,7 @@
+def personal_details():
+    name, age = "Manish Kumar", 21
+    enrollno = "e19cse102"
+    batch="eb06"
+    print("Name: {}\nAge: {}\nenrollno: {} \nBatchno: {}".format(name, age,enrollno,batch))
+
+personal_details()
